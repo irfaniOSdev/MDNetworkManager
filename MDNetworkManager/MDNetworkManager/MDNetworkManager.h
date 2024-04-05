cae8b6e24b8d2cad36e12de614eff947cf3fe9dc
Muhammad Irfan <mohammad.irfan9117@gmail.com>
2024-04-05 23:47:10 +0500

    Added framework

---
+++ MDNetworkManager/MDNetworkManager/MDNetworkManager.h
@@ -0,0 +1,18 @@
+//
+//  MDNetworkManager.h
+//  MDNetworkManager
+//
+//  Created by Muhammad Irfan on 05/04/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MDNetworkManager.
+FOUNDATION_EXPORT double MDNetworkManagerVersionNumber;
+
+//! Project version string for MDNetworkManager.
+FOUNDATION_EXPORT const unsigned char MDNetworkManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MDNetworkManager/PublicHeader.h>
+
+
